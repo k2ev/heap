da97a9073444724400b99a706d21cacf9182ec5b
k2ev <30880794+k2ev@users.noreply.github.com>
2017-10-08 17:08:25 -0400

    clean up and remove parition function from DEPS

--- DEPS.py
+++ DEPS.py
@@ -306,7 +306,6 @@ class heapATwin:
         else:
             return cls()
 
-
 class Deap(heapA):
     _offset = 1
 
@@ -497,25 +496,6 @@ class Deap(heapA):
             return True
         else:
             return False
-
-    @staticmethod
-    def partition(list_val, pivot_index):
-        list_size = len(list_val)
-        pivot = list_val[pivot_index]
-        # swap pivot with last element
-        list_val[pivot_index], list_val[list_size - 1] = list_val[list_size - 1], list_val[pivot_index]
-        # i is boundary of partition with smaller elements; j is boundary of partition with higher elements.
-        # in between i and j is the working area
-        i, j = 0, list_size - 1
-        while j > i:
-            if list_val[j - 1] > pivot:
-                list_val[j] = list_val[j - 1]
-                j = j - 1
-            else:
-                list_val[i], list_val[j - 1] = list_val[j - 1], list_val[i]
-                i = i + 1
-        list_val[j] = pivot
-        return j
 
     @classmethod
     def from_list(cls, list_val):
